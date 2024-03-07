4eddbf2dd50817ec7ca7c8285bfa813ec8da038b
doug chang <dougc@stanfordalumni.org>
2024-03-07 07:32:10 -0800

    add test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import dataJSON from './MOCK_DATA.json' assert { type: "json" };
+import dataJSON2 from './starwars-names.json' assert { type: "json" };
+
 import uniqueRandomArray from 'unique-random-array'
 
 export const API={
@@ -6,6 +8,14 @@ export const API={
   random: uniqueRandomArray(dataJSON)
 }
 
+export const starwarsNames={
+  data:dataJSON2,
+  random: uniqueRandomArray(dataJSON2)
+}
+
 //console.log(API.data)
 //console.log("------")
-console.log((uniqueRandomArray(API.data)()))
+//console.log((uniqueRandomArray(API.data)()))
+//export default API
+
+export default starwarsNames;
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,30 @@
 
 import { describe,it } from 'mocha'
 import {expect} from 'chai'
+import starWarsNames from './index.js'
+//import API from './index.js'
 
-expect.chai
-describe("starwars-names",function(){
-  it('should work',()=>{
-    expect(true).to.be.true
+console.log(starWarsNames.data)
+
+
+describe('starwars-names', function() {
+  it('should have a list of all available names', function() {
+    expect(starWarsNames.data).to.satisfy(isArrayOfStrings);
   });
-})
+
+  it('should allow me to get a random name from the list', function() {
+    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.data));
+  });
+});
+
+function isArrayOfStrings(array) {
+  return array.every(function(i) {
+    return typeof i === 'string';
+  });
+}
+
+function isIncludedIn(array) {
+  return function(item) {
+    return array.includes(item);
+  };
+}
