7304841194dc62439e9802fa736669be36dccc01
doug chang <dougc@stanfordalumni.org>
2024-03-06 20:32:24 -0800

    tests

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+
+import { describe,it } from 'mocha'
+import {expect} from 'chai'
+
+expect.chai
+describe("starwars-names",function(){
+  it('should work',()=>{
+    expect(true).to.be.true
+  });
+})
